cacb032208ae39202d1eb11f9ba647186c10342c
HaSeung2 <108933977+HaSeung2@users.noreply.github.com>
2022-07-31 16:09:20 +0900

    Update UserService.java

--- src/main/java/com/my/service/UserService.java
+++ src/main/java/com/my/service/UserService.java
@@ -1,9 +1,11 @@
 package com.my.service;
 
 import com.my.domain.BuyProductDTO;
+import com.my.domain.ReviewDTO;
 import com.my.domain.UserDTO;
 
 public interface UserService {
+	boolean reviewDelete(int reviewnum);
 	int checkId(String useremail);
 	boolean join(UserDTO user);
 	UserDTO login(String useremail, String userpw);
@@ -13,4 +15,7 @@ public interface UserService {
 	boolean nameCn(String useremail, String username);
 	boolean zipCn(UserDTO user);
 	boolean buyProduct(BuyProductDTO buy);
+	boolean review(ReviewDTO review);
+	int getReviewnum();
+	boolean reviewModify(ReviewDTO review);
 }
